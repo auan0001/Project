101b66568c78e3af77829b3c9335f1c70214f831
auan <august.forsman@outlook.com>
2022-03-21 22:59:43 +0100

    More report stuff

--- livedb.py
+++ livedb.py
@@ -6,6 +6,7 @@ from dash.dependencies import Input, Output, State
 import serial
 from pymongo import MongoClient
 from datetime import datetime as dt
+from datetime import date
 import numpy as np
 import time
 import pprint
@@ -21,14 +22,14 @@ from scipy.signal import savgol_filter
 # TODO add constants
 BAUD = 9600
 IP_ADR = 27017
-MINUTE_MS = 600000
+HOUR_MS = 60*600000
 PADDING = 150
 BG_COLOR = '#FFFFFF'
 
 ''' MongoDB '''
 client = MongoClient('localhost', IP_ADR)
 db = client.beertemp
-collection = db.log
+settings = db.settings
 entry = db.entries
 
 ''' **************************************** '''
@@ -42,14 +43,41 @@ app.layout = html.Div(style={'padding': PADDING, 'background' : BG_COLOR}, child
     html.H3('This is a live feed!'),
     html.Div([
         dcc.Graph(id='live-update-graph-scatter', animate=False),
-        html.H3('Filter number of entries in database'),
-        dcc.Input(id='resolution', value='2000', type='text'),
-        dcc.Graph(id='history-graph-scatter', animate=False),
+        html.Hr(),
         dcc.Interval(
             id='interval-component',
-            interval=1*MINUTE_MS
+            interval=1*HOUR_MS
             )
-        ])
+        ]),
+    html.Div([
+        html.H3('Set temperature alarm value'),
+        # dcc.Input(
+            # id="input_range_min", type="number", debounce=True, placeholder="Set min temp",
+            # min=-5, max=40, step=1, value=int(settings.find_one({"_id": "settings"})['min'])
+            # ),
+        # dcc.Input(
+            # id="input_range_max", type="number", debounce=True, placeholder="Set max temp",
+            # min=-5, max=40, step=1, value=int(settings.find_one({"_id": "settings"})['max'])
+            # ),
+        dcc.Input(
+            id="input_range_drop", type="number", debounce=True, placeholder="Temp drop tolerance",
+            min=1, max=40, step=1, value=int(settings.find_one({"_id": "settings"})['drop'])
+            ),
+        # html.Div(id="min-max-out"),
+        html.Div(id="drop-out"),
+        html.H3('Set start and end date'),
+        dcc.DatePickerRange(
+            id='date-picker-range',
+            min_date_allowed = entry.find_one()['time'].date(),
+            max_date_allowed = list(entry.find().sort('$natural',-1).limit(1))[0]['time'].date(),
+
+            # TODO timedelta based fallback
+            start_date = date(2022,2,28),
+            end_date = date(2022,3,4)
+            ),
+        dcc.Graph(id='history-graph-scatter', animate=False),
+        html.Div(id='output-container-date-picker-range')
+        ]),
     ])
 
 
@@ -87,33 +115,38 @@ def update_graph_scatter(graph_update):
 
 # TODO callback to apply Savgol filtering
 @app.callback(Output('history-graph-scatter', 'figure'),
-        Input(component_id='resolution', component_property='value'))
+        [Input(component_id='date-picker-range', component_property='start_date'),
+        Input(component_id='date-picker-range',component_property='end_date')]
+        )
 # @app.callback(Output('live-update-graph-scatter', 'figure'))
-def update_history(resolution):
+def update_history(start_date, end_date):
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    df = pd.DataFrame(list(db.entries.find().sort([('_id',-1)])))
+
+    filtered_df = df[df['time'].between(
+        dt.strftime(start_date, "%Y-%m-%d"),
+        dt.strftime(end_date, "%Y-%m-%d")
+    )]
+
     SAVGOL_WIN_LEN = 201
-    # num_filter = re.compile(r'\D')
-    FALLBACK_RES = 2000
-    # if resolution != '' and num_filter.findall(resolution) != [] \
-    if resolution != '' and int(resolution) > SAVGOL_WIN_LEN: 
-        valid_res = resolution
-    else:
-        valid_res = FALLBACK_RES
 
     try:
-        df = pd.DataFrame(list(db.entries.find().limit(int(valid_res)).sort([('$natural',-1)])))
-        trace1 = go.Scatter(
-            x=df['time'],
-            y=df['temperature'],
-            name='Beer temperature',
-            mode= 'markers',
-            marker = {'color': 'Blue',
-                'size': 3}
-            )
+        # trace1 = go.Scatter(
+            # x=df['time'],
+            # y=df['temperature'],
+            # name='Beer temperature',
+            # mode= 'markers',
+            # marker = {'color': 'Blue',
+                # 'size': 3}
+            # )
 
         trace2 = go.Scatter(
-            x=df['time'],
-            y=savgol_filter(df['temperature'], SAVGOL_WIN_LEN, 2),
-            name='Filtered signal',
+            x=filtered_df['time'],
+            # y=savgol_filter(filtered_df['temperature'], SAVGOL_WIN_LEN, 2),
+            y=filtered_df['temperature'],
+            name='Sensor signal [C]',
+            # name='Filtered signal',
             mode= 'lines+markers',
             marker = {'color': 'Red',
                 'size': 2}
@@ -128,12 +161,85 @@ def update_history(resolution):
             yaxis= {'autorange': True}
             )
 
-    fig = go.Figure(data=[trace1, trace2], layout=layout)
+    # fig = go.Figure(data=[trace1, trace2], layout=layout)
+    fig = go.Figure(data=trace2, layout=layout)
+    min_temp = filtered_df['temperature'].min() 
+    max_temp = filtered_df['temperature'].max() 
+    mean_temp = filtered_df['temperature'].mean() 
 
+    fig.add_annotation(text="Min = " + str(min_temp) + " C",
+        xref="paper", yref="paper",
+        x=0.1, y=1, showarrow=False, 
+           font=dict(
+            family="Courier New, monospace",
+            size=16,
+            color="#ffffff"
+            ),
+        bordercolor="#c7c7c7",
+        borderwidth=2,
+        borderpad=4,
+        bgcolor="red",
+        opacity=0.8
+        )
+
+    fig.add_annotation(text="Max = " + str(max_temp) + " C",
+        xref="paper", yref="paper",
+        x=0.9, y=1, showarrow=False, 
+           font=dict(
+            family="Courier New, monospace",
+            size=16,
+            color="#ffffff"
+            ),
+        bordercolor="#c7c7c7",
+        borderwidth=2,
+        borderpad=4,
+        bgcolor="red",
+        opacity=0.8
+        )
+
+    fig.add_annotation(text="Mean = " + str(round(mean_temp, 3)) + " C",
+        xref="paper", yref="paper",
+        x=0.5, y=1, showarrow=False, 
+           font=dict(
+            family="Courier New, monospace",
+            size=16,
+            color="#ffffff"
+            ),
+        bordercolor="#c7c7c7",
+        borderwidth=2,
+        borderpad=4,
+        bgcolor="red",
+        opacity=0.8
+        )
     fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='Grey')
     fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='Grey')
 
     return fig
+
+# @app.callback(
+    # Output("min-max-out", "children"),
+    # Input("input_range_min", "value"),
+    # Input("input_range_max", "value"),
+# )
+# def set_temp_min_max(rangemin, rangemax):
+    # # if rangemin or rangemax == None:
+        # # return "Temperature(s) out of range"
+    # if rangemin >= rangemax:
+        # return "Min has to be lower than max"
+    # else:
+        # settings.update_one({"_id": "settings"}, {"$set":{"min": rangemin, "max": rangemax}})
+        # return "Max temp: {}C | Min temp: {}C".format(rangemin, rangemax)
+
+@app.callback(
+    Output("drop-out", "children"),
+    Input("input_range_drop", "value"),
+)
+def set_temp_drop(rangedrop):
+    if rangedrop == None:
+        return "Temp drop out of range"
+    else:
+        settings.update_one({"_id": "settings"}, {"$set":{"drop": rangedrop}})
+        return "Warning if temperature drops >= {} C".format(rangedrop)
 
 if __name__ == '__main__':
     app.run_server(debug=True, host="0.0.0.0")
--- serial_temp_to_db.py
+++ serial_temp_to_db.py
@@ -3,8 +3,17 @@ from pymongo import MongoClient
 from datetime import datetime as dt
 import time
 import systemd.daemon
+import os.path
+import base64
+from email.mime.text import MIMEText
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-''' **************************************** '''
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
 # Iniatilzation temperature. 0C looks better than a 'trash output'.
 temp = 0
@@ -22,8 +31,8 @@ CORRECT_TESTS = 3
 ''' MongoDB '''
 client = MongoClient('localhost', IP_ADR)
 db = client.beertemp
-collection = db.log
 entry = db.entries
+settings = db.settings
 
 # Open serial connection to MCU
 ser = serial.Serial(PORT, BAUD)
@@ -66,14 +75,87 @@ def init_script(temp):
             ' correct readings and ' + str(faulty_count) + ' faulty readings')
     systemd.daemon.notify('READY=1')
 
+def detect_temp_drop():
+    # Return list of the two latest temperatures using natural ordering
+    diff_entries = list(entry.find().sort('$natural',-1).limit(2))
+
+    temp_1 = diff_entries[0]['temperature']
+    temp_2 = diff_entries[1]['temperature']
+
+    date_1 = diff_entries[0]['time'].date()
+    date_2 = diff_entries[1]['time'].date()
+   
+    # Return temp drop tolerance
+    drop_tol = int(settings.find_one({"_id": "settings"})['drop'])
+
+    # Check temp drop tolerance only if the two entries has same date
+    if date_1 == date_2:
+        temp_drop = temp_2 - temp_1
+        if temp_drop > drop_tol:
+            send_warning(drop_tol, temp_drop, date_1)
+
+def create_message(sender, to, subject, message_text):
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+def send_warning(drop_tol, temp_drop, date_1):
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('/home/alarm/token.json'):
+        creds = Credentials.from_authorized_user_file('/home/alarm/token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '/home/alarm/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)
+        to = "forsman.august@gmail.com"
+        sender = "beermail2022@gmail.com"
+        user_id = "me"
+        subject = "Beer Temperature Warning"
+        message_text = "WARNING, AUGUST!\nTemperature has dropped: " + str(temp_drop) + "C\nbetween the last two readings."
+        message = create_message(sender, to, subject, message_text)
+        # send_message(service, user_id, message)
+
+    except HttpError as error:
+        print(error)
+
+def send_message(service, user_id, message):
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print('Warning message sent')
+    print(message['id'])
+    return message
+  except HttpError as error:
+    print(error)
+
 def main():
     init_script(temp)
     while True:
-        time.sleep(60)
+        time.sleep(10)
         try:
             temp_to_db(temp)
+            detect_temp_drop()
+            print("Current temp drop parameter: ")
+            print(int(settings.find_one({"_id": "settings"})['drop']))
         except Exception as e:
             print(str(e))
+
 ''' **************************************** '''
 
 if __name__ == "__main__":
